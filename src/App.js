6c303449f59c15df7fcbfc2be30ebee3136bbc2f
duvivierbri <duvivierbri@gmail.com>
2022-05-14 13:29:49 -0400

    Made the skeleton for the HowTo page and setup BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import MainScreen from './components/Main';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HowToPage from './components/HowTo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <MainScreen/>
+      <BrowserRouter>
+        <Routes>
+
+          <Route exact path = "/" element = {
+              <MainScreen/>
+            }
+          />
+
+          <Route path = "/howtouse" element = {
+            <HowToPage/>
+          }
+          />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/HowTo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HowToPage = (props) => {
+    return (
+        <div className = "HowToPage">
+            <h1 className = "howToHeader">Instructions</h1>
+            <p className = "instructions">Instructions go here...</p>
+        </div>
+    )
+}
+
+export default HowToPage;

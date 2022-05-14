8ee205a4c3e8f72c6f931a79eb1be6446916048f
duvivierbri <duvivierbri@gmail.com>
2022-05-14 14:21:45 -0400

    Added styling files for each page and component needed so far

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -1,11 +1,12 @@
 import React from "react";
+import '../styling/HowTo.css';
 
 const HowToPage = (props) => {
     return (
         <div className = "HowToPage">
             <h1 className = "howToHeader">Instructions</h1>
             <p className = "instructions">Instructions go here...</p>
-            
+
             <a href = "/">
                 <button>Go Back</button>
             </a>

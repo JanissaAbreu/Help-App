f2187af320e8f4e224ff19e17e6c2bebd1b64612
duvivierbri <duvivierbri@gmail.com>
2022-05-14 13:49:38 -0400

    Added json files for all the suggestions for each emotion

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 const MainScreen = (props) => {
+    //This "enterButton" function is where the code for the enter button will go
+    //Right now it just console.logs "Enter button clicked"
+    const enterButton = () => {
+        console.log("Enter button clicked")
+    }
+
     return (
         <div className = "mainScreen">
             <h1 className="mainScreenTitle">TITLE</h1>
             <input className = "userInput" placeholder = "Type emotion here..." type = "text"/>
-            <button className="enterButton">Enter</button>
+            <button className="enterButton" onClick = {() => {enterButton()}}>Enter</button>
             <br/>
             <a href = "/howtouse">
                 <button className="howToButton">How To Use</button>

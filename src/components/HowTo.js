15df3a3a1312de6f88a44173bc8811179c819f8f
duvivierbri <duvivierbri@gmail.com>
2022-05-15 13:09:57 -0400

    Wrote instructions

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -4,8 +4,13 @@ import '../styling/HowTo.css';
 const HowToPage = (props) => {
     return (
         <div className = "HowToPage">
-            <h1 className = "howToHeader">Instructions</h1>
-            <p className = "instructions">Instructions go here...</p>
+            <h1 className = "howToHeader">How To Use the Help-Bot</h1>
+            <p className = "instructions">
+                Type an emotion in the textbox and press enter to get a response <br/>
+                <b>For example...</b> <br/>
+                'I'm bored' , 'I'm happy', 'I'm sad' <br/>
+                Once you hit the enter button, you'll get a response from our help-bot depending on your emotion.<br/>
+            </p>
 
             <a href = "/">
                 <button>Go Back</button>
